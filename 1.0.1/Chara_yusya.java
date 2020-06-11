22a6589eeeef37efa5ce4c579851026134e2c308
taichi00takada <taichi00takada@gmail.com>
2020-06-12 00:32:04 +0900

    commit test 01

---
+++ 1.0.1/Chara_yusya.java
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+
+package Game_ver2;
+
+public  class Chara_yusya extends Chara{
+	static String charaName = "勇者";
+	static int hp = 1000;
+    static int mp = 500;
+    static int power = 100;
+    static int defense = 50;
+
+
+}
+=======
+
+package Game_ver2;
+
+public  class Chara_yusya extends Chara{
+	static String charaName = "勇者";
+	static int hp = 1000;
+    static int mp = 500;
+    static int power = 100;
+    static int defense = 50;
+
+
+}
+>>>>>>> 86eab50e52f00ed33cc1af0735214bf8be54b10c
